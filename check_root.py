41db6e9e85994ffa4edc870b170ab4d95c0fb9dd
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2022-04-20 18:15:47 +0200

    fix: changed error message in check_root.py

--- check_root.py
+++ check_root.py
@@ -3,6 +3,6 @@ import os
 
 def user_check():
     if not 'SUDO_UID' in os.environ.keys():
-        sys.exit("This program should be executed with a sudoer account !")
+        sys.exit("This program should be executed with a sudoer account during the first start !")
     else:
         print("User is root")
