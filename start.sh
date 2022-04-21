a907780350299b133e7072a1307bf4035f15b8ed
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2022-04-21 20:01:41 +0200

    feat: start.sh now installs MySql server by itself

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 FILE=./installed
 if [ -f "$FILE" ]
 then
@@ -15,8 +17,14 @@ else
     apt install python3
   fi
 
-  touch ./PHOTOS.json
   python3 ./first_install.py
+
+  sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password iutphoto"
+  sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password iutphoto"
+  sudo apt-get update
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mariadb-server
+
+  touch ./PHOTOS.json
   if [ $? -eq 1 ]
   then
     kill $$
