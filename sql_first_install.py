9d9c189659cf512ef1b9c4d87debcbc980a37739
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2022-04-26 10:10:12 +0200

    fix: move a parameter in sql_first_install.py

--- sql_first_install.py
+++ sql_first_install.py
@@ -1,19 +1,19 @@
 import mysql.connector
 import sql_bridge
 
-def Db_Initialise():
+def Db_Initialise(new_password):
   Database = mysql.connector.connect(
     host="localhost",
     user="root",
     password="iutphoto"
   )
   Db_Cursor = Database.cursor()
-  new_password = "test"
   sql = "ALTER USER 'root'@'localhost' IDENTIFIED BY '"+new_password+"';"
   Db_Cursor.execute(sql)
   Database.commit()
 
-Db_Initialise()
+
+Db_Initialise("test")
 
 sql_bridge.Db_Connection_Start()
 sql_bridge.Db_Change_SQL_File("test","/Users/jean-baptistebruneau/Downloads/mael_1","root","Iutphoto123@")
