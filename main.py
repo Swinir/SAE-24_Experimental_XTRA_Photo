0ad3c6cea37ea1ee1fbcb7e36421d999acda3d65
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2022-04-28 19:32:54 +0200

    feat: added first sql select for testing

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import save_data
 
 import photo_taker
-
+import sql_bridge
 
 import atexit
 
+
+
+
+sql_bridge.Db_Connection_Start()
 
 #------------------------------------------------------------------------------------------
 
@@ -13,9 +17,8 @@ import atexit
 ask = input(str("Take a pic ?"))
 if ask == "yes":
     photo_taker.take_picture()
-    photo_taker.take_picture()
 
-
+print(sql_bridge.Db_Select("logs","id_log,contenue_log",""))
 
 #------------------------------------------------------------------------------------------
 
@@ -25,6 +28,8 @@ if ask == "yes":
 def exit_handler():
     print ('App is exiting')
     save_data.save_class(photo_taker.PHOTOS_container,"PHOTOS.json")
+    file = open("PHOTOS.json", "w")
+    file.close()
 
 
 atexit.register(exit_handler)
