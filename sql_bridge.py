ced7c0e85033f10d8cb8ea07a386c4a4a826f837
Jean-Baptiste Bruneau <34415232+Swinir@users.noreply.github.com>
2022-04-28 12:53:29 +0200

    feat: added Db select function

--- sql_bridge.py
+++ sql_bridge.py
@@ -2,23 +2,32 @@ import mysql.connector
 import subprocess
 
 def Db_Connection_Start():
-  global Database
-  Database = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Iutphoto123@"
-  )
+      global Database
+      Database = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Iutphoto123@"
+      )
 
-  #print(Database)
-  global Db_Cursor
-  Db_Cursor = Database.cursor()
+      #print(Database)
+      global Db_Cursor
+      Db_Cursor = Database.cursor()
 
 def Db_Change_SQL_File(db_name,path,user,passwd):
-  sql = str("mysql -u "+user+" --password="+passwd+" "+db_name+" < "+path+".sql")
-  subprocess.call(sql, shell=True)
+      sql = str("mysql -u "+user+" --password="+passwd+" "+db_name+" < "+path+".sql")
+      subprocess.call(sql, shell=True)
 
 def Db_Insert(str_to_insert,table,fields,types):
-  sql = str("INSERT INTO "+table+" ("+fields+") VALUES ("+types+")")
-  val = str_to_insert
-  Db_Cursor.executemany(sql, val)
-  Database.commit()
+      sql = str("INSERT INTO "+table+" ("+fields+") VALUES ("+types+")")
+      val = str_to_insert
+      Db_Cursor.executemany(sql, val)
+      Database.commit()
+
+def Db_Select(table,fields,conditions):
+      if conditions:
+            sql = str("SELECT " + fields + " FROM " + table+" WHERE "+conditions)
+      else:
+            sql = str("SELECT "+fields+" FROM "+table)
+      Db_Cursor.execute(sql)
+      result = Db_Cursor.fetchall()
+      return result
