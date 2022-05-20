24867d9ebc0f135204d2d378eda80fcd02b765a8
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2022-05-20 13:10:38 +0200

    feat: crontab trigger for php

---
+++ crontab_php_enable.py
@@ -0,0 +1,25 @@
+from crontab import CronTab
+import sys
+
+f = open('installed_path.txt', 'r')
+path = f.read()
+f.close()
+
+cron = CronTab(tabfile='/etc/crontab', user=False)
+command = 'python3 ' + str(path)
+
+def cron_en():
+    job = cron.new(command=command)
+    job.day.every(1)
+    cron.write()
+
+
+def cron_dis():
+    cron.remove_all(command)
+    cron.write()
+
+
+if sys.argv[0] == "activate":
+    cron_en()
+elif sys.argv[0] == "disable":
+    cron_dis()
