133d6a2360e2083aa6e37ddef9528fc4abb3464e
Mael Albany <99243661+Walou974@users.noreply.github.com>
2022-06-04 13:09:57 +0200

    fix: errors about taking photos
    - Prise de photo et mise à jour de la BDD actualisées;
    - Nouvelle var $_SESSION['user_conn'] pour connaitre l'utilisateur connecté;
    - Restriction des accés au page de paramètres (via restri.php);

--- html/change_mdp.php
+++ html/change_mdp.php
@@ -1,4 +1,34 @@
 <?php
 include("base.php");
-echo 'nouvelle page'
+
 ?>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Changer de mot de passe</title>
+</head>
+<body>
+    <?php
+    include("header.php");
+    include("menu.php");
+    include("restri.php");
+    ?>
+    <section id="main">
+        <div id="mdp">
+            <form action="fin_mdp.php">
+                <label for="amdp">Ancien mot de passe du compte</label><br>
+                <input type="text" name="ancien_mdp" id="amdp"><br>
+
+                <label for="nmdp">Nouveau mot de passe</label><br>
+                <input type="text" name="nouveau_mdp" id="nmpd"><br>
+
+                <label for="cmdp">Confirmer le nouveau mot de passe</label><br>
+                <input type="text" name="conf_mdp" id="cmdp"><br>
+
+                <input type="submit" value="Valider">
+            </form>
+        </div>
+    </section>
+</body>
+</html>
--- html/deco.php
+++ html/deco.php
@@ -3,5 +3,6 @@ session_start();
 $_SESSION['connecte'] = 0;
 $_SESSION['tentative'] = 0;
 $_SESSION['admin'] = 0;
+$_SESSION['user_conn'] = null;
 header('Location: index.php')
 ?>
