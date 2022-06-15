b06650595a707fd680da1291f52567453570cae6
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2022-06-15 13:18:14 +0200

    fix: changed password hashing for admin

--- html/fin_mdp.php
+++ html/fin_mdp.php
@@ -18,12 +18,13 @@ $req->execute();
 $mdp2 = $req->fetchall();
 $req->closeCursor();
 $mdp = $mdp2[0]['password'];
-echo $mdp;
-echo $pwd_old;
+#echo $mdp;
+#echo $pwd_old;
 /* Comparaison de l'ancien mdp de la base avec celui du formulaire */
 if($mdp2[0]['id_user']==1){
-    if($mdp2[0]['id_user']==$pwd_old){
+    if($mdp2[0]['password']==$pwd_old || password_verify($pwd_old,$mdp)){
         if($pwd_new == $pwd_cfd){
+            $pwd_new = password_hash($pwd_new, PASSWORD_DEFAULT);
             $sql = "UPDATE `users` SET `password` = '".$pwd_new."' WHERE `users`.`login` = '".$login."'";
             $req = $bd->prepare($sql);
             $req->execute();
@@ -33,6 +34,7 @@ if($mdp2[0]['id_user']==1){
             header("Location: change_mdp.php?err=1");
         }
     }else{
+        print_r($mdp2);
         header("Location: change_mdp.php?err=2");
     }
 }else{
